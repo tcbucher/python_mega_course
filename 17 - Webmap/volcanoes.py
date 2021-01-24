b2d349492f98af5badc8f346212a308dcc0d373a
tcbucher <tcbucher@gmail.com>
2021-01-24 12:12:16 -0500

    Volcano map
    updated popup message

--- 17 - Webmap/volcanoes.py
+++ 17 - Webmap/volcanoes.py
@@ -3,13 +3,15 @@ import pandas
 
 volc_data = pandas.read_csv('Volcanoes.txt')
 
-lat = list(volc_data['LAT']) 
+lat = list(volc_data['LAT'])
 lon = list(volc_data['LON'])
 names = list(volc_data['NAME'])
+elevations = list(volc_data['ELEV'])
 
-feat_group = folium.FeatureGroup(name='volcanoes')      
-for x, y, name in zip(lat, lon, names):
-    feat_group.add_child(folium.Marker(location=(x,y), popup=name))
+feat_group = folium.FeatureGroup(name='volcanoes')
+for x, y, name, el in zip(lat, lon, names, elevations):
+    msg = name + '\n' + str(el) # TODO: fstring
+    feat_group.add_child(folium.Marker(location=(x,y), popup=msg))
 
 map_us = folium.Map(location=(38,-96), zoom_start=5)
 map_us.add_child(feat_group)
