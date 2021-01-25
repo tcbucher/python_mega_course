43fc7ab6424571a88ec62184f9d26c1040617e72
Tim Bucher <tcbucher@gmail.com>
2021-01-25 13:57:30 -0500

    Volcano map
    Added colorful circle markers to indicate elevation of each volcano.  Downloaded and unzipped some JSON  geographic data containing global country boundaries

--- 17 - Webmap/volcanoes.py
+++ 17 - Webmap/volcanoes.py
@@ -9,9 +9,20 @@ names = list(volc_data['NAME'])
 elevations = list(volc_data['ELEV'])
 
 feat_group = folium.FeatureGroup(name='volcanoes')
+
+def getMarkerColor(elev):
+    if elev <= 1000:
+        marker_color = '#00FF00'
+    elif elev <= 3000:
+        marker_color = '#FFAA00'
+    else:
+        marker_color = '#FF0000'
+    return marker_color
+
 for x, y, name, el in zip(lat, lon, names, elevations):
-    msg = name + '\n' + str(el) # TODO: fstring
-    feat_group.add_child(folium.Marker(location=(x,y), popup=msg))
+    msg = f'{name}\n{el}'
+
+    feat_group.add_child(folium.CircleMarker(location=(x,y), popup=msg, radius=7, fill=True, color='#777777', fill_color=getMarkerColor(el), opacity=0.6, fill_opacity=0.6))
 
 map_us = folium.Map(location=(38,-96), zoom_start=5)
 map_us.add_child(feat_group)
